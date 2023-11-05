eaeb2a580178f304df21bfdb4361e3843cc7a80a
egidijus <145454567+egidijus1989@users.noreply.github.com>
2023-11-05 17:16:43 +0200

    namai

--- JS/js.js
+++ JS/js.js
@@ -82,7 +82,7 @@ let knyguKatalogas =
 
 for (let kategorija in knyguKatalogas){
     console.log(`${kategorija} (${knyguKatalogas[kategorija].length} knygos)`);
-    console.log("---------------")
+    console.log("---------------");
     for(knyga of knyguKatalogas[kategorija]){
         console.log(`ISBN: ${knyga.ISBN}`);
         if (knyga.leidimoMetai == 2023){
@@ -94,5 +94,76 @@ for (let kategorija in knyguKatalogas){
         console.log(`Pavadinimas: ${knyga.pavadinimas}`);
         console.log(`Puslapiu skaicius: ${knyga.puslapiuSkaicius}`);
         console.log("---------------")
+    }
+}
+
+
+let head = document.querySelector('.accordion-button')
+let body = document.querySelector('.accordion-body');
+for (let kategorija in knyguKatalogas){
+    let category = document.createElement('h2');
+    category.textContent = `${kategorija} (${knyguKatalogas[kategorija].length} knygos)`;
+    head.appendChild(category);
+    for(knyga of knyguKatalogas[kategorija]){
+        let book1 = document.createElement('p');
+        book1.textContent = `ISBN: ${knyga.ISBN}`;
+        body.appendChild(book1);
+        if (knyga.leidimoMetai == 2023){
+            let book2 = document.createElement('p');
+            book2.textContent = `Leidimo metai: ${knyga.leidimoMetai} ------- Nauja Knyga`;
+            body.appendChild(book2);
+        }
+        else{
+            let book2 = document.createElement('p');
+            book2.textContent = `Leidimo metai: ${knyga.leidimoMetai}`;
+            body.appendChild(book2);
+        }
+        let book3 = document.createElement('p');
+        book3.textContent = `Pavadinimas: ${knyga.pavadinimas}`;
+        body.appendChild(book3);
+        let book4 = document.createElement('p');
+        book4.textContent = `Puslapiu skaicius: ${knyga.puslapiuSkaicius}`;
+        body.appendChild(book4);
+        let book5 = document.createElement('p');
+        book5.textContent = `---------------------------------`;
+        body.appendChild(book5);
+    }
+}
+let accordion = document.querySelector('.accordion');
+for (let kategorija in knyguKatalogas){
+    let category = document.createElement('h2');
+    let accordionItem = document.createElement('div.accordion-item');
+    let accordionHeader = document.createElement('h2.accordion-header');
+    let accordionButton = document.createElement('button.accordion-button');
+    let accordionBody = document.createElement('div.accordion-body');
+    category.textContent = `${kategorija} (${knyguKatalogas[kategorija].length} knygos)`;
+    accordionButton.appendChild(category);
+    accordionHeader.appendChild(accordionButton);
+    accordionItem.appendChild(accordionHeader);
+    accordionItem.appendChild(accordionBody);
+    accordion.appendChild(accordionItem);
+    for(knyga of knyguKatalogas[kategorija]){ 
+        let book1 = document.createElement('p');
+        book1.textContent = `ISBN: ${knyga.ISBN}`;
+        accordionBody.appendChild(book1);
+        if (knyga.leidimoMetai == 2023){
+            let book2 = document.createElement('p');
+            book2.textContent = `Leidimo metai: ${knyga.leidimoMetai} ------- Nauja Knyga`;
+            accordionBody.appendChild(book2);
+        }
+        else{
+            let book2 = document.createElement('p');
+            book2.textContent = `Leidimo metai: ${knyga.leidimoMetai}`;
+            accordionBody.appendChild(book2);
+        }
+        let book3 = document.createElement('p');
+        book3.textContent = `Pavadinimas: ${knyga.pavadinimas}`;
+        accordionBody.appendChild(book3);
+        let book4 = document.createElement('p');
+        book4.textContent = `Puslapiu skaicius: ${knyga.puslapiuSkaicius}`;
+        accordionBody.appendChild(book4);
+        let book5 = document.createElement('p');
+        book5.textContent = `---------------------------------`;
+        accordionBody.appendChild(book5);
     }
 }
